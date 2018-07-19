88e90f6c3c3cee77744a77156eb3a88d33855bdc
RafalKostecki <edward-12@o2.pl>
2018-07-19 13:52:23 +0200

    Add logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
+import { MenuComponent } from './menu/menu.component';
+import { SectionTitleComponent } from './section-title/section-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    MenuComponent,
+    SectionTitleComponent
   ],
   imports: [
     BrowserModule,
