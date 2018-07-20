fb7a661015621d0878d173019b2d2b6e3ff6c90f
RafalKostecki <edward-12@o2.pl>
2018-07-20 12:55:55 +0200

    Improve portfolio

--- src/app/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio-project/portfolio-project.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-project',
   templateUrl: './portfolio-project.component.html',
   styleUrls: ['./portfolio-project.component.styl']
 })
-export class PortfolioProjectComponent implements OnInit {
+
+export class PortfolioProjectComponent {
+
+  @Input() type: string;
+  @Input() image: string;
+  @Input() link: string;
+  @Input() title: string;
+  @Input() content: string;
+  @Input() technologies: Array<string>;
+  @Input() date: string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs/Observable';
 import { DataService } from '../data.service';
 
 
@@ -7,14 +9,25 @@ import { DataService } from '../data.service';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.styl']
 })
+
+@Injectable()
 export class PortfolioComponent implements OnInit {
 
+  public portfolioProjects: Object;
   private categoryName: string = "Portfolio";
 
-  constructor(private data: DataService) { }
+  constructor(private http: HttpClient, private data: DataService) {
+    this.getJSON().subscribe(data => {
+        this.portfolioProjects = data;
+        console.log(this.portfolioProjects[0].technology)
+    });
+  }
 
   ngOnInit() {
     this.data.changeCategory(this.categoryName)
   }
 
+  public getJSON(): Observable<any> {
+    return this.http.get("../assets/json/portfolio.json")
+  }
 }
