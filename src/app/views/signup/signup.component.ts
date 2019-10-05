0e35951afc426eef0c5b41c5b03abfbcb0556bde
Rafał Kostecki <edward-12@o2.pl>
2019-10-05 20:20:06 +0200

    Create sign up form

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../../../assets/scripts/must-match.validadator';
 
 @Component({
   selector: 'app-signup',
@@ -9,11 +11,34 @@ import { DataService } from '../../data.service';
 export class SignupComponent implements OnInit {
 
   private categoryName: string = "Sign up";
+  registerForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(private data: DataService) { }
+  constructor(private formBuilder: FormBuilder, private data: DataService) { }
 
   ngOnInit() {
-    this.data.changeCategory(this.categoryName)
+    this.data.changeCategory(this.categoryName);
+
+    this.registerForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+        validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  get formControls() { 
+    return this.registerForm.controls; 
+  }
+
+  onSubmit() {
+      this.submitted = true;
+
+      if (this.registerForm.invalid) return;
+
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
   }
 
 }
