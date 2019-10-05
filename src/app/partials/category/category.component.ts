4909fe5733eeeaf1bd2b82c702879c1c8a84d353
Rafał Kostecki <edward-12@o2.pl>
2019-10-05 19:06:12 +0200

    Create components for sign in and sign up

---
+++ src/app/partials/category/category.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../data.service';
+
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.styl']
+})
+export class CategoryComponent implements OnInit {
+
+  public currentCategory: string;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.category.subscribe(cat => this.currentCategory = cat)
+  }
+
+}
