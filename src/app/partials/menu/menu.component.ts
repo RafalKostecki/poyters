39667d2872540c028e595e726a324789d51bd23e
Rafał Kostecki <edward-12@o2.pl>
2019-10-19 22:40:01 +0200

    Menu auto close after changing page

--- src/app/partials/menu/menu.component.ts
+++ src/app/partials/menu/menu.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.styl']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
   private menu: any = document.querySelector('.menu')
-  private menuNav: any = document.querySelector('#menuNav')
+  
   constructor() { }
 
-  ngOnInit() {
-    this.menuNav = document.querySelector('#menuNav')
-    this.menuNav.addEventListener('click', () => this.clicked())
-  }
-
-  clicked(){
+  toggleMenu(){
     this.menu = document.querySelector('.menu')
     this.menu.classList.toggle('menu--visible');
   }
