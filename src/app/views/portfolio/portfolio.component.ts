a2885afedabdc0be20ec7b20d61f8d917b05c3a1
Rafał Kostecki <edward-12@o2.pl>
2020-02-14 13:45:15 +0100

    POY-43 upgrade angular

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { UiService } from '../../services/ui.service';
 
 
