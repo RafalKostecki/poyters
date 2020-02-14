5b1e6d3912c5aa28c542c212338b50962240e2c9
Rafał Kostecki <edward-12@o2.pl>
2020-02-14 11:07:41 +0100

    POY-44 create base views of login and portfolio panels

---
+++ src/app/partials/profil-panel/profil-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilPanelComponent } from './profil-panel.component';
+
+describe('ProfilPanelComponent', () => {
+  let component: ProfilPanelComponent;
+  let fixture: ComponentFixture<ProfilPanelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProfilPanelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfilPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/partials/profil-panel/profil-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-panel',
+  templateUrl: './profil-panel.component.html',
+  styleUrls: ['./profil-panel.component.styl']
+})
+export class ProfilPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
