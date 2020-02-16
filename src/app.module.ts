5e76922570b2c83808084d9f9d0ac64382374c46
Rafał Kostecki <edward-12@o2.pl>
2020-02-16 16:29:15 +0100

    POY-29 create users and auth modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 const accessString = 'mongodb+srv://new-test-user:9OFB838GLJY0h1vx@cluster0-amydc.mongodb.net/test?retryWrites=true&w=majority';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot(accessString)
+    MongooseModule.forRoot(accessString),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
