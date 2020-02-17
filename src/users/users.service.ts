24c6bedecc3ca57a2fe73c8dea552eba01997758
Rafał Kostecki <edward-12@o2.pl>
2020-02-17 18:57:36 +0100

    POY-29 can create new user in DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,35 +1,53 @@
 
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { IUser } from './user.model';
 
 
 @Injectable()
 export class UsersService {
-  private readonly users: IUser[];
+  // private readonly users: IUser[];
+  private users: IUser[] = [];
 
-  constructor() {
-    this.users = [
-      {
-        userId: 1,
-        username: 'john',
-        password: 'changeme',
-      },
-      {
-        userId: 2,
-        username: 'chris',
-        password: 'secret',
-      },
-      {
-        userId: 3,
-        username: 'maria',
-        password: 'guess',
-      },
-      {
-        userId: 4,
-        username: 'test',
-        password: 'password'
-      }
-    ];
+  constructor(@InjectModel('User') private readonly userModel: Model<IUser>) {
+    // this.users = [
+    //   {
+    //     userId: 1,
+    //     username: 'john',
+    //     password: 'changeme',
+    //     mail: 'test@mail.com'
+    //   },
+    //   {
+    //     userId: 2,
+    //     username: 'chris',
+    //     password: 'secret',
+    //     mail: 'test@mail.com'
+    //   },
+    //   {
+    //     userId: 3,
+    //     username: 'maria',
+    //     password: 'guess',
+    //     mail: 'test@mail.com'
+    //   },
+    //   {
+    //     userId: 4,
+    //     username: 'test',
+    //     password: 'password',
+    //     mail: 'test@mail.com'
+    //   }
+    // ];
+  }
+
+  async insertUser(username: string, password: string, mail: string) {
+    const newUser = new this.userModel({
+        username,
+        password,
+        mail
+    });
+
+    const result = await newUser.save();
+    return result.id
   }
 
   async findOne(username: string): Promise<IUser | undefined> {
