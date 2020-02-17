31ad5e3a6c46c2177195d9c52f11c4978b4c203e
Rafał Kostecki <edward-12@o2.pl>
2020-02-17 18:30:03 +0100

    POY-29 add UserSchema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.model';
+import { UsersController } from './users.controller';
+
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Users', schema: UserSchema}])],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
