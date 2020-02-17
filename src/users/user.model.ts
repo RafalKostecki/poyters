b9e97a95c2b21efa849cbafad345ebe942c9802c
Rafał Kostecki <edward-12@o2.pl>
2020-02-17 19:55:31 +0100

    POY-29 add bcrypt to protect passwords

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -9,7 +9,6 @@ export interface IUser extends mongoose.Documen {
 };
 
 export const UserSchema = new mongoose.Schema({
-  // _id: mongoose.Schema.Types.ObjectId,
   username: { type: String, required: true },
   password: { type: String, required: true },
   mail: { type: String, required: true }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IUser } from './user.model';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require("bcrypt");
 
 
 @Injectable()
@@ -40,14 +42,34 @@ export class UsersService {
   }
 
   async insertUser(username: string, password: string, mail: string) {
-    const newUser = new this.userModel({
-        username,
-        password,
-        mail
-    });
+    const existingUsers = await this.userModel.find({username: username}).exec();
 
-    const result = await newUser.save();
-    return result.id
+    console.log('existingUser', existingUsers)
+    
+    if (existingUsers.length > 0) {
+      throw new HttpException({
+        status: 409,
+        error: 'User already exists',
+      }, 409);
+    } else {
+      await bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          throw new HttpException({
+            status: 500,
+            error: 'Error during crypting a password',
+          }, 500);
+        } else {
+          const newUser = new this.userModel({
+            username,
+            password: hash,
+            mail
+          });
+  
+          console.log(newUser)
+          newUser.save();
+        }
+      });
+    }
   }
 
   async findOne(username: string): Promise<IUser | undefined> {
