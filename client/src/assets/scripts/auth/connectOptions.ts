aa7179fd1e3188a3537acc4f81a1b9eb10a76fd7
Rafał Kostecki <edward-12@o2.pl>
2020-02-28 16:08:17 +0100

    POY-57 create logout

---
+++ client/src/assets/scripts/auth/connectOptions.ts
@@ -0,0 +1,8 @@
+export const connectOptions = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Credentials': 'true'
+  },
+  credentials: 'include'
+}
