e6fd153b85ceb84c21314a9fc43d0f81b78919dc
Rafał Kostecki <edward-12@o2.pl>
2020-02-29 02:35:12 +0100

    POY-58 create auth guard with http request

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,25 +1,27 @@
 import { UserService } from '../services/user.service';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { map, take, catchError} from 'rxjs/operators';
-import { corsHeaders } from '../../assets/scripts/auth/connectOptions';
+import { Observable, of } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { map, catchError} from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private userService: UserService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>  {
+  canActivate(): Observable<boolean>  {
     return this.userService.isLoggedIn().pipe(
       map(res => {
-        console.log(res);
         if (res) {
           return true;
         } else {
           this.router.navigate(['/signin']);
           return false;
         }
+      }),
+      catchError(() => {
+        this.router.navigate(['/signin']);
+        return of(false);
       })
     );
   }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { corsHeaders } from '../../assets/scripts/auth/connectOptions';
 import { HttpClient } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
