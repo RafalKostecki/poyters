b7fac7b02175ec2ac2fa9bece08ef7faa722de15
Rafał Kostecki <edward-12@o2.pl>
2020-08-03 20:22:50 +0200

    POY-38 fix problem with footer

--- client/src/app/components/credits-footer/credits-footer.component.ts
+++ client/src/app/components/credits-footer/credits-footer.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-credits-footer',
   templateUrl: './credits-footer.component.html',
   styleUrls: ['./credits-footer.component.styl']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
-  public currentYear: number = new Date().getFullYear()
+  public currentYear: number = new Date().getFullYear();
+  public currentCategory: string;
+
+  constructor(private uiService: UiService) { }
+
+  ngOnInit() {
+    this.uiService.category.subscribe(cat => this.currentCategory = cat)
+  }
   
+  get footerStyles () {
+    console.log(this.currentCategory)
+		return {
+      position: this.currentCategory === 'Contact' ? 'fixed' : 'relative'
+    }
+  };
 }
