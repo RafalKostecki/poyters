dac5108c9e8c4d8480385249b63fa2119a44babe
Rafał Kostecki <edward-12@o2.pl>
2020-11-04 01:10:39 +0100

    AC-38 create mail module

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Response, Body, Post, UseGuards, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+
 
 @Controller('mail')
-export class MailController {}
+export class MailController {
+  constructor(
+    private readonly mailService: MailService
+  ) {}
+
+  @Post('send')
+  async sendMail(
+    @Response() res,
+    @Body('from') from: string, 
+    @Body('to') to: string, 
+    @Body('subject') subject: string,
+    @Body('text') text: string
+  ) {
+    try {
+      const mailId: string = await this.mailService.send(from, to, subject, text);
+      res.status(200).send(`Mail ${mailId} sent`);
+    } catch (error) {
+      console.log(error)
+      res.status(500).send('Internal server error');
+    }  
+  }
+}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import nodemailer from 'nodemailer';
+import smtpTransport from 'nodemailer-smtp-transport';
+
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  async send (from: string, to: string, subject: string, text: string): Promise<string> {
+    const transporter = nodemailer.createTransport(smtpTransport({
+      host: "mail49.mydevil.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "no-reply@poyters.pl",
+        pass: "pass"
+      },
+      connectionTimeout: 5000, // No effect
+      greetingTimeout: 5000, // No effect
+      socketTimeout: 5000 // No effect
+    }));
+  
+    console.log('here 1')
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from,
+      to,
+      subject,
+      text
+    });
+
+    console.log(`Message ${info} sent`)
+
+    return info?.messageId;
+  }
+}
