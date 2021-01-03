2f4e20ba129b1addd990dc01e74041d817593206
Rafał Kostecki <edward-12@o2.pl>
2021-01-03 22:04:37 +0100

    POY-94 style slider pagination

--- client/src/app/components/slider/slider.component.ts
+++ client/src/app/components/slider/slider.component.ts
@@ -11,28 +11,57 @@ export class SliderComponent implements OnInit {
 
   public slides = [
     {
-      mediaPath: 'path to img', 
+      mediaPath: 'https://www.wykop.pl/cdn/c3201142/comment_1608277727me3r599LODkzUdghahpkay,w400.jpg', 
       type: 'IMAGE', // IMAGE | VIDEO
       alt: "Alternative text 1"
     },
     {
-      mediaPath: 'path to img', 
+      mediaPath: 'https://www.wykop.pl/cdn/c3201142/comment_1608286999Y8l0G6G4yRYg2rIU98a4qb,w400.jpg', 
       type: 'IMAGE', // IMAGE | VIDEO
       alt: "Alternative text 2"
     },
     {
-      mediaPath: 'path to img', 
+      mediaPath: 'https://www.wykop.pl/cdn/c3201142/comment_1608282503PTngFbLYS46Hqh0tdLF0FY,w400.jpg', 
       type: 'IMAGE', // IMAGE | VIDEO
       alt: "Alternative text 3"
     },
     {
-      mediaPath: 'path to img', 
+      mediaPath: 'https://www.wykop.pl/cdn/c3201142/comment_1608253076fn5oeC2MCd9yOAZmjljgp7,w400.jpg', 
       type: 'IMAGE', // IMAGE | VIDEO
       alt: "Alternative text 4"
     }
   ]
 
+  public currentSlide = 0;
+
   ngOnInit(): void {
+  }
+
+  prev() {
+    if (this.currentSlide === 0) return;
+
+    this.currentSlide -= 1;
+
+    console.log('prev', this.currentSlide)
+  }
+
+  next() {
+    if (this.currentSlide === this.slides.length - 1) return;
+
+    this.currentSlide += 1;
+
+    console.log('next', this.currentSlide)
+  }
+
+  get tapeStyles () {
+    const tapeElement: any = document.getElementById('sliderTape');
+
+    const tapeWidth = parseInt(getComputedStyle(tapeElement).width);
+
+    console.log(tapeWidth)
+    return {
+      left: `${(this.currentSlide * tapeWidth) * (-1)}px`
+    }
   }
 
 }
